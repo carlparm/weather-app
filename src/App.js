bf6a1ae12e33a44ef881e2f43d85df0637b8663a
Carl <carlparm@gmail.com>
2020-08-23 16:59:06 -0400

    date box

--- src/App.js
+++ src/App.js
@@ -5,11 +5,28 @@ import React from 'react';
 // }
 
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day}, ${month} ${date}, ${year}`
+  }
+
   return (
     <div className="app">
       <main>
         <div className="search-box">
           <input type="text" className="search-bar" placeholder="Where are you?" />
+        </div>
+        <div className="location-box">
+          <div className="location">Washington, DC</div>
+          <div className="date">{dateBuilder(new Date())}</div>
         </div>
       </main>
     </div>

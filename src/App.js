d90c94fb7f8a29c40b33dae0b1cb866c9d681957
Carl <carlparm@gmail.com>
2020-08-23 16:11:35 -0400

    background added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import './App.css';
-const api = {
-  key: '53ceecdbb29519cd8bf11ca375ed4f75',
-  base: "https://api.openweathermap.org/data/2.5"
-}
+// const api = {
+//   key: '53ceecdbb29519cd8bf11ca375ed4f75',
+//   base: "https://api.openweathermap.org/data/2.5"
+// }
 
 function App() {
-
   return (
-    <div className="App">
+    <div className="app">
       <main>
         <div className="search-box">
           <input type="text" className="search-bar" placeholder="Where are you?" />

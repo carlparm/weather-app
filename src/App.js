110b8a0b44aa87e818d08b05600820937e5dee93
Carl <carlparm@gmail.com>
2020-08-23 15:40:43 -0400

    udpated api key and url

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 const api = {
   key: '53ceecdbb29519cd8bf11ca375ed4f75',
-  base: "api.openweathermap.org/data/2.5/weather?lat={38.8813}&lon={77.0281}&appid={53ceecdbb29519cd8bf11ca375ed4f75}"
+  base: "https://api.openweathermap.org/data/2.5"
 }
 
 function App() {

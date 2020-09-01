42e43b7151b87566f93a547fe503b647394525f3
Carl <carlparm@gmail.com>
2020-09-01 19:51:45 -0400

    App to class

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
 const api = {
   key: '53ceecdbb29519cd8bf11ca375ed4f75',
-
 }
 
-function App() {
+class App extends React.Component {
 
-  const [query, setQuery] = useState('')
-  const [weather, setWeather] = useState({})
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
 
-  const search = evt => {
-    if (evt.key === "Enter") {
+  componentDidMount() {
       fetch(`api.openweathermap.org/data/2.5/weather?lat=38.879700&lon=-77.025820&appid=${api.key}`)
         .then(res => res.json())
         .then(result => {
-          setWeather(result);
-          setQuery('');
-          console.log(weather);
+          console.log(result);
         });
-    }
   }
 
-  const dateBuilder = (d) => {
+  dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
@@ -33,23 +31,25 @@ function App() {
     return `${day}, ${month} ${date}, ${year}`
   }
 
-  return (
-    <div className="app">
-      <main>
-        <div className="search-box">
-          <input type="text" className="search-bar" placeholder="Where are you?" onChange={e => setQuery(e.target.value)} value={query} onKeyPress={search}/>
-        </div>
-        <div className="location-box">
-          <div className="location">Washington, DC</div>
-          <div className="date">{dateBuilder(new Date())}</div>
-        </div>
-        <div className="weather-box">
-          <div className="temp">15c</div>
-          <div className="weather">Sunny</div>
-        </div>
-      </main>
-    </div>
+  render() {
+    return (
+      <div className="app">
+        <main>
+          <div className="search-box">
+            {/* <input type="text" className="search-bar" placeholder="Where are you?" onChange={e => setQuery(e.target.value)} value={query} onKeyPress={search}/> */}
+          </div>
+          <div className="location-box">
+            <div className="location">Washington, DC</div>
+            <div className="date">{this.dateBuilder(new Date())}</div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">15c</div>
+            <div className="weather">Sunny</div>
+          </div>
+        </main>
+      </div>
   );
+}
 }
 
 export default App;
